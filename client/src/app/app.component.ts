3ce83f7aae9dde0eedc6b7a15d81e70360caaa3c
Huỳnh Chiến <chienhuynh234@gmail.com>
2024-11-25 01:51:21 +0700

    add missing package and fix minor error

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  http = inject(HttpClient);
+  users: any;
+
+  ngOnInit(): void { 
+    this.http.get('https://localhost:5001/api/user').subscribe({
+      next: reponse => this.users = reponse,
+      error: error => console.error('There was an error!', error),
+      complete: () => console.log('Completed!')
+    });
+  }
 }
