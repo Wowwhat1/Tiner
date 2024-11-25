c8a8341fda0f5dd6ef277fab0623f6b1404253f9
Huỳnh Chiến <chienhuynh234@gmail.com>
2024-11-25 18:06:04 +0700

    implement login to client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,26 @@ import { NavComponent } from "./nav/nav.component";
 })
 export class AppComponent implements OnInit {
   title = 'client';
+  private accountService = inject(AccountService);
   http = inject(HttpClient);
   users: any;
 
   ngOnInit(): void { 
+    this.getUser();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      const user: any = JSON.parse(userString);
+      this.accountService.currentUser.set(user);
+    } else {
+      return;
+    }
+  }
+
+  getUser() {
     this.http.get('https://localhost:5001/api/user').subscribe({
       next: reponse => this.users = reponse,
       error: error => console.error('There was an error!', error),
